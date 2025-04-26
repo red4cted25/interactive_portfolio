d758b2b31a301af8cdab0e9af1c4981e558c8353
Nico <141285419+red4cted25@users.noreply.github.com>
2025-04-25 23:21:55 -0700

    Update AboutMe.jsx
    Updated AboutMe.jsx with a small description. It is temporarily under Nico's Computer until a NicoDiaz.exe app on the desktop is developed

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -4,12 +4,11 @@ const AboutMeContent = () => (
     <Frame
         variant="field"
         className="about-me-frame my-4 p-4"
+        style={{ display: 'flex', gap: '16px', flexDirection: 'column'}}
     >
-        <p>Hey! I'm Nicolas, or Nico, a junior full stack web developer with a passion for creating unique and engaging user experiences.</p>
-        <br />
-        
-        <br />
-        <p>When I'm not coding, you can find me gaming, reading, learning cybersecurity, or researching random topics that cross my mind.</p>
+        <p>Hey! I'm Nico Diaz, a freshman at Arizona State University and a junior full stack web developer with a passion for creating unique and engaging user experiences.</p>
+        <p>Through my experience with West-MEC, I've developed projects with HTML, CSS, JS, ReactJS, and NextJS.</p>
+        <p>When I'm not coding or doing university homework, you can find me gaming, reading, listening to music, or researching random topics that cross my mind.</p>
     </Frame>
 );
 
