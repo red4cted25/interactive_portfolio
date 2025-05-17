95f5963c48fc33309097f35dd4b32548541af20f
Nico <141285419+red4cted25@users.noreply.github.com>
2025-05-17 12:26:56 -0700

    Fix index.js
    Fixed index.js so AppStarter works properly

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Win98Portfolio from './Win98Home';
+import AppStarter from './AppStarter';
 
 const preloadResources = () => {
     const sounds = [
